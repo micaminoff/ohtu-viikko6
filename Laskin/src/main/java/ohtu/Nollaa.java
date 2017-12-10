9171608a5b7ee3d9231b1652dc225697750f04af
Michael Aminoff <michael@noviaria.com>
2017-12-10 18:44:52 +0200

    Refactoring

---
+++ Laskin/src/main/java/ohtu/Nollaa.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author squid
+ */
+class Nollaa implements Komento {
+    private Sovelluslogiikka sovellus;
+    private JTextField tuloskentta;
+    private JTextField syotekentta;
+
+    public Nollaa(Sovelluslogiikka sovellus, JTextField tuloskentta, JTextField syotekentta) {
+        this.sovellus = sovellus;
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+    }
+
+    @Override
+    public void suorita() {
+        sovellus.nollaa();
+        this.syotekentta.setText("");
+        this.tuloskentta.setText(""+sovellus.tulos());
+    }
+    
+}
